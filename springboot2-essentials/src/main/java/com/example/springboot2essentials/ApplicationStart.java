35fdc01ab4883869b669bbb8f3f197f1021752c4
Irina Ferreira <irina.f@aluno.ifsp.edu.br>
2023-04-26 03:48:48 -0300

    Finalizando aula 6

--- springboot2-essentials/src/main/java/com/example/springboot2essentials/ApplicationStart.java
+++ springboot2-essentials/src/main/java/com/example/springboot2essentials/ApplicationStart.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@SpringBootApplication
+//@Configuration //criam filtros para seguranca
+//@EnableAutoConfiguration
+@SpringBootApplication //ela já inclui as annotations acima
 public class ApplicationStart {
 
 	public static void main(String[] args) {
--- springboot2-essentials/src/main/java/com/example/springboot2essentials/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/com/example/springboot2essentials/controller/AnimeController.java
@@ -2,6 +2,7 @@ package com.example.springboot2essentials.controller;
 
 import com.example.springboot2essentials.domain.Anime;
 import com.example.springboot2essentials.util.DateUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,17 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("anime")
 @Log4j2
+@RequiredArgsConstructor //essa annotation elimina escrever o construtor dentro da classe
 public class AnimeController {
 
-
+//@Autowired ->faz um injeção de dependencia, mas não é correto deixar ele neste local
     private DateUtil dateUtil;
 
-    public AnimeController(DateUtil dateUtil) {
+  /*  public AnimeController(DateUtil dateUtil) {//utilizando esse construtor, é feito uma injeçao de dependencia
         this.dateUtil = dateUtil;
     }
+    A maneira correta de criar uma injeçao de dependencia é utilizando uma interface*/
 
     @GetMapping("list")
     public List<Anime> list(){
