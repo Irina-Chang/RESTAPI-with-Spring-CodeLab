4a4ac35fbac6774c9ffc529d0664405b63908387
Irina Ferreira <irina.f@aluno.ifsp.edu.br>
2023-04-26 04:10:57 -0300

    Alterando diretorios

---
+++ springboot2-essentials/src/main/java/codelab/spring/springboot2/controller/AnimeController.java
@@ -0,0 +1,35 @@
+package codelab.spring.springboot2.controller;
+
+import com.example.springboot2essentials.domain.Anime;
+import com.example.springboot2essentials.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor //essa annotation elimina escrever o construtor dentro da classe
+public class AnimeController {
+
+//@Autowired ->faz um injeção de dependencia, mas não é correto deixar ele neste local
+    private DateUtil dateUtil;
+
+  /*  public AnimeController(DateUtil dateUtil) {//utilizando esse construtor, é feito uma injeçao de dependencia
+        this.dateUtil = dateUtil;
+    }
+    A maneira correta de criar uma injeçao de dependencia é utilizando uma interface*/
+
+    @GetMapping("list")
+    public List<Anime> list(){
+
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("DB2"), new Anime("Berserk"));}
+}
+
---
+++ springboot2-essentials/src/test/java/codelab/spring/springboot2/Springboot2EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package codelab.spring.springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2EssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
