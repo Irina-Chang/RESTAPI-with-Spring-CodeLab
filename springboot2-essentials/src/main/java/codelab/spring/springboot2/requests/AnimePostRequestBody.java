9344dfac01cc38ce943ceb12dcdd273022cd5e5c
Irina Ferreira <irina.f@aluno.ifsp.edu.br>
2023-05-06 04:12:10 -0300

    Criando testes de Integração

--- springboot2-essentials/src/main/java/codelab/spring/springboot2/requests/AnimePostRequestBody.java
+++ springboot2-essentials/src/main/java/codelab/spring/springboot2/requests/AnimePostRequestBody.java
@@ -2,12 +2,16 @@ package codelab.spring.springboot2.requests;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty!")
     @NotNull
